fca144af3f449fc8a076640b3df327dd7e05d6c8
Soorajhari <soorajhari1999@gmail.com>
2024-02-13 23:47:03 +0530

    axios changes

--- Client/crud-task/src/utils/axios.js
+++ Client/crud-task/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://crud-task-rl73.vercel.app/",
 
   headers: {
     "Content-Type": "application/json",
