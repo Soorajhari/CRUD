b3b370ed0bf5b21fa12e3808722803e13a5e20d1
Soorajhari <soorajhari1999@gmail.com>
2024-02-13 23:17:07 +0530

    mongo url change

--- Server/utils/db.js
+++ Server/utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectToMongoDB() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/TaskData', {
+    await mongoose.connect('mongodb+srv://soorajhari1999:4plvEWdaf5Vrq1ud@cluster0.d3scysj.mongodb.net/?myDatabaseretryWrites=true&w=majority', {
       
     });
         console.log('Connected to MongoDB hi every');
